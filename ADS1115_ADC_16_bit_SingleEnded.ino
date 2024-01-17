96d6047a3ac2db70906b21858f3f16fd4e4638bf
Vedant K. Naik <vnaik792014@gmail.com>
2024-01-16 22:35:18 -0500

    Update ADS1115_ADC_16_bit_SingleEnded.ino

--- ADS1115_ADC_16_bit_SingleEnded.ino
+++ ADS1115_ADC_16_bit_SingleEnded.ino
@@ -1,28 +1,31 @@
 #include "Arduino.h"
 #include "Wire.h"
 #define I2Caddress 0x48 // 1001000-GND addr
-
-// Running average
-// unsigned int readings[20] = {0};
-// unsigned char readCnt = 0;
-
+#define clockFrequency 400000 //fast clock i2c
+#include <TimerOne.h>
 void setup()
 {
 	Serial.begin(9600);
 	Wire.begin();
+	Wire.setClock(clockFrequency);
 	Serial.println("Setup completed.");
 	Wire.beginTransmission(I2Caddress);
 	Wire.write(0b00000001);
 	Wire.write(0b00000010);
 	Wire.write(0b11100010);
 	Wire.endTransmission();
+	Timer1.initialize(16667); // 60Hz sampling
+	Timer1.attachInterrupt(sendDatatoSerial); // attach the callback function   
 }
 void loop()
-{
+{	
+}
+void sendDatatoSerial () {
 	Wire.beginTransmission(I2Caddress);
 	Wire.write(0b00000000);
 	Wire.endTransmission();
 	Wire.requestFrom(I2Caddress, 2);
 	uint16_t convertedValue;
 	convertedValue = (Wire.read() << 8 | Wire.read());
+	Serial.println(convertedValue);
 }
